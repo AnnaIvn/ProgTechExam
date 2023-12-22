dfb6da761514e437ae960439034c719722134f7c
Ivanytska Anna <aniaiv12345@gmail.com>
2023-12-22 10:56:03 +0200

    changed app.py and testing.py

--- app.py
+++ app.py
@@ -7,39 +7,14 @@ def index():
     result = arithmetic_progression(n)
     return f"Result: {result}"
 
-# обчислює суму перших n членів арифметичної прогресії: Xn = 2, 4, 6 ...
+# обчислює суму перших n членів арифметичної прогресії: Xn = 1, 5, 9 ...
 def arithmetic_progression(n):
-    print("Ivanytska Anna")
     if n < 0:
         raise Exception("Inapropriate input (try values bigger than '-1')")
-    a1 = 2  # first term of progression
-    d = 2   # step
+    a1 = 1  # first term of progression
+    d = 4   # step
     sum = n * (2*a1 + d*(n-1)) // 2
     return sum
-
-# обчислює суму перших n членів геометричної прогресії: 
-def geometric_progression(n):
-    if n < 0:
-        raise Exception("Inapropriate input (try values bigger than '-1')")
-    b1 = 2    # first term of progression
-    q = 0.5   # denominator of the progression (знаменник)
-    if q == 0:
-        raise Exception("q can't be 0. Try other value")
-    sum = b1 * (q**n - 1) / (q - 1)
-    return sum
-
-# рахує, скільки днів лишилося до дня народження. Формат вводу: yyyy-mm-dd
-from datetime import datetime
-def days_til_birthday(birthdate, today=None):
-    if today is None:
-        today = datetime.now()
-    next_birthday = datetime(today.year, birthdate.month, birthdate.day)  # date of birth in today year
-
-    if today > next_birthday:   # comparing today date with date of birth in today year, if we already had birthday
-        next_birthday = datetime(today.year + 1, birthdate.month, birthdate.day)   # se set a new year for our birthday
-
-    days_left = (next_birthday - today).days  # counting days to our birthday 
-    return days_left
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
--- testing.py
+++ testing.py
@@ -1,6 +1,5 @@
 import unittest
 import app as app
-from datetime import datetime
 
 class TestArithmeticProgression(unittest.TestCase):
     
@@ -11,49 +10,15 @@ class TestArithmeticProgression(unittest.TestCase):
     # first command
     def test_arithmetic_progression_sum(self):
         self.assertEqual(app.arithmetic_progression(0), 0)    # comparing values (input with result)
-        self.assertEqual(app.arithmetic_progression(1), 2)
+        self.assertEqual(app.arithmetic_progression(1), 1)
         self.assertEqual(app.arithmetic_progression(2), 6)
-        self.assertEqual(app.arithmetic_progression(4), 20)
-        self.assertEqual(app.arithmetic_progression(11), 132)
+        self.assertEqual(app.arithmetic_progression(3), 15)
+        self.assertEqual(app.arithmetic_progression(4), 28)
+        self.assertEqual(app.arithmetic_progression(11), 231)
 
     def test_arith_sum_exception(self):
         with self.assertRaises(Exception):
             app.arithmetic_progression(-1)   # returns true = exception occurs
-
-
-    # second command
-    def test_geometric_progression_sum(self):
-        self.assertEqual(app.geometric_progression(0), 0)   # comparing values (input with result)
-        self.assertEqual(app.geometric_progression(1), 2)
-        self.assertEqual(app.geometric_progression(2), 3)
-        self.assertEqual(app.geometric_progression(3), 3.5)
-        self.assertEqual(app.geometric_progression(4), 3.75)
-
-    def test_geometric_sum_exception(self):
-        with self.assertRaises(Exception):
-            app.geometric_progression(-10)   # returns true = exception occurs
-
-
-    # third command
-    def test_days_til_birthday_next_year(self):  # birthday another year
-        birthdate = datetime(2024, 6, 1)
-        today = datetime(2023, 12, 19)
-        self.assertEqual(app.days_til_birthday(birthdate, today), 165)  
-    
-    def test_days_til_birthday_today(self):  # birthday today
-        birthdate = datetime(2023, 12, 19)
-        today = datetime(2023, 12, 19)
-        self.assertEqual(app.days_til_birthday(birthdate, today), 0)   
-    
-    def test_days_til_birthday_this_year(self):  # birthday this year
-        birthdate = datetime(2023, 12, 19)
-        today = datetime(2023, 1, 19)
-        self.assertEqual(app.days_til_birthday(birthdate, today), 334)
-
-    def test_days_til_birthday_that_has_been(self):  # birthday that has already passed, one year mus be added
-        birthdate = datetime(2023, 2, 19)
-        today = datetime(2023, 12, 19)
-        self.assertEqual(app.days_til_birthday(birthdate, today), 62)
 
 
     def tearDown(self):    # tear down
